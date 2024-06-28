6b19a68b7a552079ca146aa8bf6567bb2a71cdc3
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-06-28 14:28:51 +0300

    application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,11 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    application
+}
+
+application {
+    mainClass = "hexlet.code.App"
 }
 
 group = "hexlet.code"
@@ -10,6 +16,10 @@ repositories {
 }
 
 dependencies {
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    compileOnly ("org.projectlombok:lombok:1.18.32")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
