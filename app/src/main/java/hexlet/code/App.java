358b8aa9f02b39a8bdb8cbffde13a5988232225f
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-06-28 14:34:15 +0300

    методы getApp + main

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,22 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+
+public class App {
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        app.get("/", ctx -> ctx.result("Hello, World"));
+        return app;
+    }
+
+    public static void main(String[] args) {
+        var app = getApp();
+        app.start(7070);
+    }
+}
+
