7f7c3f0fe18d0cf1c87afaecbe992b30a084aa4e
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-07-01 15:27:24 +0300

    модель Url

---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,17 @@
+package hexlet.code.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Url {
+    private int id;
+    private String name;
+    private Timestamp createdAt;
+}
