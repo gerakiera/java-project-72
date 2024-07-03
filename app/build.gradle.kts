b632490cf6ef10214488ee49c8098524d7b1829c
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-07-03 19:44:26 +0300

    konghq и squareup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,13 +18,18 @@ repositories {
 dependencies {
     implementation("io.javalin:javalin-rendering:6.1.3")
     implementation("io.javalin:javalin:6.1.3")
+    implementation("io.javalin:javalin-bundle:6.1.3")
     implementation("gg.jte:jte:3.1.12")
     implementation("org.slf4j:slf4j-simple:2.0.7")
     compileOnly ("org.projectlombok:lombok:1.18.32")
     implementation("com.h2database:h2:2.2.224")
     implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("org.postgresql:postgresql:42.7.2")
+    implementation("org.jsoup:jsoup:1.17.2")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("com.konghq:unirest-java:3.13.0")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
 }
 
 tasks.test {
