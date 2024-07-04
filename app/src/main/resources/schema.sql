714ea27418040549156ec25f1b12d20e1d16e818
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-07-04 15:57:05 +0300

    таблица url_checks

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,5 +1,19 @@
+DROP TABLE IF EXISTS url_checks;
+DROP TABLE IF EXISTS urls;
+
 CREATE TABLE urls (
 id BIGINT PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(255) NOT NULL,
 created_at TIMESTAMP NOT NULL
-)
+);
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    status_code INTEGER NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    h1 VARCHAR(255) NOT NULL,
+    description text NOT NULL,
+    url_id INT NOT NULL,
+    FOREIGN KEY (url_id) REFERENCES urls(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
