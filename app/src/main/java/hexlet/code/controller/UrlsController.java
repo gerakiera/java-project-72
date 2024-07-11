41715250e4097c72b8e96c022021e37ad61ce483
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-07-11 17:20:05 +0300

    AllArgsConstructor

--- app/src/main/java/hexlet/code/controller/UrlsController.java
+++ app/src/main/java/hexlet/code/controller/UrlsController.java
@@ -1,14 +1,19 @@
 package hexlet.code.controller;
 
-import hexlet.code.dto.MainPage;
+import hexlet.code.dto.BasePage;
 import hexlet.code.dto.UrlPage;
 import hexlet.code.dto.UrlsPage;
 import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
 import hexlet.code.repository.UrlChecksRepository;
 import hexlet.code.repository.UrlsRepository;
 import hexlet.code.utils.NamedRoutes;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -19,15 +24,15 @@ import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class UrlsController {
     public static void root(Context ctx) {
-        var flash = ctx.consumeSessionAttribute("flash");
-        var page = new MainPage((String) flash);
+        var page = new BasePage();
         page.setFlash(ctx.consumeSessionAttribute("flash"));
         ctx.render("index.jte", model("page", page));
     }
     public static void index(Context ctx) throws SQLException {
         var page = new UrlsPage(UrlsRepository.getEntities(),
-                ctx.consumeSessionAttribute("flash"),
                 UrlChecksRepository.findLatestChecks());
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("flashType"));
         ctx.render("urls/index.jte", model("page", page));
     }
 
@@ -35,9 +40,10 @@ public class UrlsController {
         var id = ctx.pathParamAsClass("id", Long.class).get();
         var url = UrlsRepository.find(id)
                 .orElseThrow(() -> new NotFoundResponse("Url not found"));
-        //url.setUrlChecks(UrlChecksRepository.getEntitiesByUrlId(id));
-        String flash = ctx.consumeSessionAttribute("flash");
-        var page = new UrlPage(url, flash);
+        url.setUrlChecks(UrlChecksRepository.getEntitiesByUrlId(id));
+        var page = new UrlPage(url);
+        page.setFlash(ctx.consumeSessionAttribute("flash"));
+        page.setFlashType(ctx.consumeSessionAttribute("flashType"));
         ctx.render("urls/show.jte", model("page", page));
     }
 
@@ -57,5 +63,32 @@ public class UrlsController {
             ctx.sessionAttribute("flash", "Некоректный URL");
             ctx.redirect(NamedRoutes.rootPath());
         }
+    }
+    public static void check(Context ctx) throws SQLException {
+        var urlId = ctx.pathParamAsClass("id", Long.class).get();
+        var url = UrlsRepository.find(urlId)
+                .orElseThrow(() -> new NotFoundResponse("Url not found"));
+        try {
+            HttpResponse<String> response = Unirest.get(url.getName()).asString();
+            var codeResponse = response.getStatus();;
+            var doc = Jsoup.parse(response.getBody());
+            var title = doc.title();
+            Element h1Element = doc.selectFirst("h1");
+            var h1 = h1Element == null ? "" : h1Element.ownText();
+
+            Element descriptionEl = doc.selectFirst("meta[name=description]");
+            var description = descriptionEl == null ? "" : descriptionEl.attr("content");
+
+            var urlCheck = new UrlCheck(codeResponse, title, h1, description);
+            urlCheck.setUrlId(url.getId());
+            UrlChecksRepository.save(urlCheck);
+            ctx.sessionAttribute("flash", "Страница успешно проверена");
+            ctx.sessionAttribute("flashType", "correct");
+        } catch (Exception e) {
+            ctx.sessionAttribute("flash", "Некорректный адрес");
+            ctx.sessionAttribute("flashType", "error");
+            ctx.redirect(NamedRoutes.urlPath(urlId));
+        }
+
     }
 }
