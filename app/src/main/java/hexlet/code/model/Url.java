ccbab4e4a1ddd2c60abdc4306d36584d2fd4a4c3
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-07-11 17:21:02 +0300

    правки

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -5,16 +5,18 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Timestamp;
+import java.util.Deque;
 
 @Setter
 @Getter
 @AllArgsConstructor
 public class Url {
     private Long id;
-    private String name;
+    private final String name;
     private Timestamp createdAt;
+    private Deque<UrlCheck> urlChecks;
 
-    public Url(long id, String name) {
+    public Url(Long id, String name) {
         this.id = id;
         this.name = name;
     }
